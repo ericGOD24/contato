328e5afe9957c93b0dc489c3f1cad684c2b05c45
ericGOD24 <ericobandeira@hotmail.com>
2018-11-09 14:53:36 -0200

    alterando alguns dados

--- erico/src/Components/Contato/Contato.js
+++ erico/src/Components/Contato/Contato.js
@@ -12,21 +12,21 @@ const Contato = () => {
             <h1 className="title" >Entre em contato</h1>
             <div className="linha">
                 <img className="contato-icon" src={whatsapp} alt="whatsapp-icon"/>
-                <h4 className="contato-text" >+55(61)98294-7102</h4>
+                <h4 className="contato-text" >+55(61)98330-3025</h4>
             </div>
             <div className="linha">
                 <img className="contato-icon" src={telegram} alt="telegram-icon"/>
-                <h4 className="contato-text" >@Feozineo</h4>
+                <h4 className="contato-text" >@EricoBandeira</h4>
             </div>
             <div className="linha">
                 <img className="contato-icon" src={carta} alt="envelope-icon"/>
-                <h4 className="contato-text" >pheofo@gmail.com</h4>
+                <h4 className="contato-text" >ericobandeira@hotmail.com</h4>
             </div>
             <div className="github-linkedin">
-                <a href={`https://github.com/Phe0`} >
+                <a href={`https://github.com/ericGOD24`} >
                     <img className="contato-icon icon-hover" src={require('../../img/logos/github-logo.png')} alt="github-logo" ></img>
                 </a>
-                <a href={`https://linkedin.com/in/pedrofeo`} >
+                <a href={`https://www.linkedin.com/in/EricoBandeira/`} >
                     <img className="contato-icon icon-hover" src={require('../../img/logos/linkedin-white.png')} alt="github-logo" ></img>
                 </a>
             </div>
