8562b3022fd20d27a4c7c8bccd6e452be224de2a
ericGOD24 <ericobandeira@hotmail.com>
2018-11-30 01:24:50 -0200

    Updates

---
+++ precache-manifest.b62848a961eea9ba308a397f33d305c9.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6d05bc7533d57b8ec41ccd7c6b1e30b",
+    "url": "/contato/static/media/Angular-white.c6d05bc7.png"
+  },
+  {
+    "revision": "c934adbd5d782df811c7",
+    "url": "/contato/static/css/main.2b8d9cc9.chunk.css"
+  },
+  {
+    "revision": "e156458a980608146368",
+    "url": "/contato/static/js/1.e156458a.chunk.js"
+  },
+  {
+    "revision": "5c82db91010818d36320",
+    "url": "/contato/static/js/runtime~main.5c82db91.js"
+  },
+  {
+    "revision": "bfc53b6a6bee1a0b135dddd79dd8468d",
+    "url": "/contato/static/media/seta2.bfc53b6a.svg"
+  },
+  {
+    "revision": "426d7c1e4fcc3ddb5bf0a0f445ac7e5e",
+    "url": "/contato/static/media/telegram-white.426d7c1e.png"
+  },
+  {
+    "revision": "cd445a07e3139f50fb6f8ac88e8a96e1",
+    "url": "/contato/static/media/whatsapp-white.cd445a07.png"
+  },
+  {
+    "revision": "516a67d197cc06d03e32996f5ef98a8c",
+    "url": "/contato/static/media/linkedin-white.516a67d1.png"
+  },
+  {
+    "revision": "470fe80387eb92fc9c91298a4d6e5501",
+    "url": "/contato/static/media/arrow-white.470fe803.svg"
+  },
+  {
+    "revision": "bdeaeef56210c1e0a5893a873b704449",
+    "url": "/contato/static/media/erico.bdeaeef5.bin"
+  },
+  {
+    "revision": "c934adbd5d782df811c7",
+    "url": "/contato/static/js/main.c934adbd.chunk.js"
+  },
+  {
+    "revision": "d9f03fbbe1017ced49070baa9be29159",
+    "url": "/contato/static/media/C++-white.d9f03fbb.png"
+  },
+  {
+    "revision": "2c1b9de75efdbff5ae8eb8d79de45acf",
+    "url": "/contato/static/media/Javascript-white.2c1b9de7.png"
+  },
+  {
+    "revision": "e17c671f1f4e8fe88f039e2980712cc5",
+    "url": "/contato/static/media/Python-white.e17c671f.png"
+  },
+  {
+    "revision": "8b02a882042585f358b6623a861ae601",
+    "url": "/contato/static/media/React-white.8b02a882.png"
+  },
+  {
+    "revision": "2b9e76a5674df54c7c628bc8e76a1f92",
+    "url": "/contato/static/media/Ruby on Rails-white.2b9e76a5.png"
+  },
+  {
+    "revision": "c8a780bd29be508cb5f63ac72fc3040d",
+    "url": "/contato/static/media/Sql-white.c8a780bd.png"
+  },
+  {
+    "revision": "43e56f44b346ed69b62dd9ec2aaa0dfd",
+    "url": "/contato/static/media/php-white.43e56f44.png"
+  },
+  {
+    "revision": "a95dd3db72a4f8c61705ae627259bf19",
+    "url": "/contato/static/media/gitlab-logo.a95dd3db.png"
+  },
+  {
+    "revision": "5b3a60cb948f817243eeea2fc444a1ef",
+    "url": "/contato/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9b3dc0ed93b8052fa4b286355e1ee9ca.js"
+  "/contato/precache-manifest.b62848a961eea9ba308a397f33d305c9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/contato/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
